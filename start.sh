4c538da19a4a05566a202982744a4a6be19c0eec
samdam3000 <samdam3000@gmail.com>
2025-05-10 11:29:45 +1200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-python app.py
+
+# Install dependencies (optional safety)
+pip install -r requirements.txt
+
+# Start the app using gunicorn on localhost:8000
+gunicorn app:app --bind 0.0.0.0:8000
